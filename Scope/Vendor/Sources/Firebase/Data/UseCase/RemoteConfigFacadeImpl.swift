abc4c340bcf2eecc5492e88fbd2bd3670accf5c2
devyhan <devyhan93@gmail.com>
2021-12-27 15:41:16 +0900

    FirebaseRemoteConfig, FirebaseABTesting import #2
    - firebase remoteconfig를 사용하기위한 준비 및 remote config관련 firebase vendor UseCase추가

---
+++ Scope/Vendor/Sources/Firebase/Data/UseCase/RemoteConfigFacadeImpl.swift
@@ -0,0 +1,19 @@
+//
+//  RemoteConfigFacadeImpl.swift
+//  Vendor
+//
+//  Created by 조요한 on 2021/12/27.
+//  Copyright © 2021 com.devyhan93.scope. All rights reserved.
+//
+
+import FirebaseRemoteConfig
+
+final class RemoteConfigFacadeImpl: RemoteConfigFacade {
+  init() {
+    
+  }
+  
+  func fetch() {
+    //
+  }
+}
---
+++ Scope/Vendor/Sources/Firebase/Domain/UseCase/RemoteConfigFacade.swift
@@ -0,0 +1,13 @@
+//
+//  RemoteConfigFacade.swift
+//  Vendor
+//
+//  Created by 조요한 on 2021/12/27.
+//  Copyright © 2021 com.devyhan93.scope. All rights reserved.
+//
+
+import Combine
+
+public protocol RemoteConfigFacade {
+  func fetch()
+}
